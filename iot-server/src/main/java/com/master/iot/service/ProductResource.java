82e4c7b91634965ceddb3fe77d32f1d83f9e5393
Vilmar Ricken <vilmar.ricken@gmail.com>
2017-06-22 00:56:45 -0300

    commit

--- iot-server/src/main/java/com/master/iot/service/ProductResource.java
+++ iot-server/src/main/java/com/master/iot/service/ProductResource.java
@@ -1,5 +1,8 @@
 package com.master.iot.service;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -17,38 +20,37 @@ import javax.ws.rs.core.MediaType;
 import com.master.iot.IOTController;
 import com.master.iot.IOTManager;
 
-@Path("products")
+@Path("controllers")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-public class ProductResource {
+public class ProductResource implements Serializable {
 
 	@GET
-	public Collection<IOTController> list() {
-		return IOTManager.getInstance().list();
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Teste list() {
+		Teste t = new Teste();
+		t.setName("XX");
+		return t;
 	}
-
 	/*
-	@PUT
-	public Product edit(@NotNull Product product)  {
-		return service.edit(product);
-	}
-
-	@GET
-	public List<Product> list(@QueryParam("search") String search) {
-		return service.list(search);
-	}
-
-	@GET
-	@Path("{id}")
-	public Product find(@NotNull @PathParam("id") Long id) throws AppException {
-		return service.find(id);
-	}
-
-	@DELETE
-	@Path("{id}")
-	public void delete(@NotNull @PathParam("id") Long id) throws AppException {
-		service.delete(id);
-	}
-	*/
+	 * @GET public Collection<IOTController> list() { return
+	 * IOTManager.getInstance().list(); }
+	 * 
+	 * @PUT public Product edit(@NotNull Product product) { return
+	 * service.edit(product); }
+	 * 
+	 * @GET public List<Product> list(@QueryParam("search") String search) {
+	 * return service.list(search); }
+	 * 
+	 * @GET
+	 * 
+	 * @Path("{id}") public Product find(@NotNull @PathParam("id") Long id)
+	 * throws AppException { return service.find(id); }
+	 * 
+	 * @DELETE
+	 * 
+	 * @Path("{id}") public void delete(@NotNull @PathParam("id") Long id)
+	 * throws AppException { service.delete(id); }
+	 */
 
 }
