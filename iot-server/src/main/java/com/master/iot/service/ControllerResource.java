46c13f91e897e47a60dcb39b32e2744b3433224a
Vilmar Ricken <vilmar.ricken@gmail.com>
2017-07-20 00:32:39 -0300

    commit

--- iot-server/src/main/java/com/master/iot/service/ControllerResource.java
+++ iot-server/src/main/java/com/master/iot/service/ControllerResource.java
@@ -6,11 +6,13 @@ import java.util.Collection;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import com.master.iot.IOTController;
 import com.master.iot.IOTManager;
+
 
 @Path("controllers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -23,6 +25,31 @@ public class ControllerResource implements Serializable {
 		IOTManager instance = IOTManager.getInstance();
 		return instance.list();
 	}
+
+	@GET
+	@Path("on/{component}/{port}")
+	public void on(@PathParam("component") String component,
+			@PathParam("port") int port) {
+		IOTManager instance = IOTManager.getInstance();
+		try {
+			instance.on(component, (byte) port);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@GET
+	@Path("off/{component}/{port}")
+	public void off(@PathParam("component") String component,
+			@PathParam("port") int port) {
+		IOTManager instance = IOTManager.getInstance();
+		try {
+			instance.off(component, (byte) port);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	/*
 	 * @GET public Collection<IOTController> list() { return
 	 * IOTManager.getInstance().list(); }
