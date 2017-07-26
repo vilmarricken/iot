0cfc3c7d6ad0def84ee71894c3916c695339e2f8
Vilmar.Ricken <vilmar.ricken@gmail.com>
2017-07-26 17:57:41 -0300

    commit

--- iot-server/WebContent/js/master-core.js
+++ iot-server/WebContent/js/master-core.js
@@ -1,3 +1,17 @@
-/**
- * 
- */
+masterRequest = function(request) {
+	if(!request.error){
+		request.error = materRequestErrorDefault;
+	}
+	if(!request.success){
+		request.success = materRequestSuccessDefault;
+	}
+	$.ajax(request);
+}
+
+materRequestErrorDefault = function(xhr, status, error){
+	alert('Error: ' + error);
+}
+
+materRequestSuccessDefault = function(result,status,xhr){
+	alert('Success: ' + result);
+}
