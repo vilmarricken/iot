75343979f29fc3ccf467798a4fdf5c0cb7a3b363
Vilmar Ricken <vilmar.ricken@gmail.com>
2019-02-26 14:27:54 -0300

    commit

--- iot/src/main/webapp/js/app.js
+++ iot/src/main/webapp/js/app.js
@@ -4,31 +4,46 @@ function Application () {
 	this.form_content = document.getElementById("div-form-content");
 }
 
-Application.prototype.addNewForm = function( _element, _state, _model ) {
-	this.addForm( new Form( _element, _state, _model ) );
+Application.prototype.addNewForm = function( _state, _model ) {
+	this.addForm( new Form( _state, _model ) );
 };
 
 Application.prototype.addForm = function( _form ) {
-	actual = _form;
-	forms.push( actual );
+	if( this.actual ) {
+		this.form_content.removeChild( this.actual.getElement() );
+	}
+	this.actual = _form;
+	this.forms.push( this.actual );
+	this.form_content.appendChild(this.actual.getElement()); 
 };
 
 Application.prototype.closeForm = function() {
 	if( this.forms.length == 0 ) {
 		this.form_content.innerHTML = "&nbsp";
 	} else {
-		
+		form = forms.pop();
+		this.form_content.innerHTML = "&nbsp";
 	}
 };
 
-function Form (_element, _state, _model) {
-	this.element = _element;
+function Form ( _state, _model ) {
 	this.state = _state;
 	this.model = _model;
+	this.build();
+}
+
+Form.prototype.getElement = function() {
+	return this.element;
 }
 
 Form.prototype.build = function() {
-	
+	form = document.createElement("div");
+	form.innerHTML = this.state;
+	this.element = form;	
 };
 
-var application = new Application();
+var application;
+
+function start() {
+	application = new Application();	
+}
